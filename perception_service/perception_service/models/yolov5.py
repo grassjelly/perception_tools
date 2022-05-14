449b4bfdb2cfef115b24b153320e2541d52e6709
Juan Jimeno <jimenojmm@gmail.com>
2022-05-15 01:35:05 +0800

    moved model config to a yaml file

--- perception_service/perception_service/models/yolov5.py
+++ perception_service/perception_service/models/yolov5.py
@@ -1,14 +1,29 @@
 from yolov5_ros.inference import Yolov5
+import yaml
 
 
 class ObjectFinder:
-    def __init__(self, **kwargs):
+    def __init__(self, yaml_config):
+        with open(yaml_config, "r") as stream:
+            try:
+                config = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        weights = config['yolov5']['weights']
+        data = config['yolov5']['data']
+        imgsz = config['yolov5']['image_size']
+        conf_thres = config['yolov5']['confidence_threshold']
+        iou_thres = config['yolov5']['iou_threshold']
+        device = config['yolov5']['gpu_id']
+
         self._model = Yolov5(
-            weights=kwargs['weights'],
-            data=kwargs['data'],
-            imgsz=kwargs['image_size'],
-            conf_thres=kwargs['conf_thres'],
-            iou_thres=kwargs['iou_thres']
+            weights=weights,
+            data=data,
+            imgsz=imgsz,
+            conf_thres=conf_thres,
+            iou_thres=iou_thres, 
+            device=device
         )
 
     def find_objects(self, img):
